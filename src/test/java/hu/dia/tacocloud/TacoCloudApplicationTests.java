26a106abf5d934f7083f12c303ffd0a5e330aa97
Diána Szántó <szanto.diana.1992@gmail.com>
2022-03-22 08:40:01 +0100

    home page and home controller and homecontroller test are created

---
+++ src/test/java/hu/dia/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.dia.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
